a27e66678802f5ee64a0eb4d9813fbff93fa99db
Agassin <129422340+Agassin@users.noreply.github.com>
2025-04-29 16:23:21 +0200

    modif de dossier

---
+++ src/model/Affaire.java
@@ -0,0 +1,55 @@
+package src.model;
+
+public class Affaire {
+    private String nomAffaire;
+    private String crime;
+    private Personne suspect;
+    private Personne coupable;  // Peut être null si aucun coupable trouvé
+    private String lieu;
+    private String etat;
+    private String date;
+
+    public Affaire(String nomAffaire, String crime, Personne suspect, Personne coupable, String lieu, String etat, String date) {
+        this.nomAffaire = nomAffaire;
+        this.crime = crime;
+        this.suspect = suspect;
+        this.coupable = coupable; // Peut être null
+        this.lieu = lieu;
+        this.etat = etat;
+        this.date = date;
+    }
+
+    public Personne getCoupable() {
+        return coupable;
+    }
+
+    public Personne getSuspect() {
+        return this.suspect; 
+    }
+
+
+    public String getEtat() {
+        return etat;
+    }
+
+    @Override
+    public String toString() {
+        return "Affaire: " + nomAffaire + " | Crime: " + crime + " | Lieu: " + lieu + " | État: " + etat;
+    }
+
+    public String getNomAffaire() {
+        return nomAffaire;
+    }
+
+    public String getLieu() {
+        return this.lieu;
+    }
+
+    public String getCrime() {
+        return this.crime;
+    }
+
+    public Object getDate() {
+        return this.date;
+    }
+}
